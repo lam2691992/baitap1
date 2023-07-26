e2edc5b4b04e872fbc2cb9b71ffd615f6d2199e0
lam2691992 <139439393+lam2691992@users.noreply.github.com>
2023-07-26 11:43:47 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# baitap1dartcoban
+ baitap1dartcoban
---
+++ bin/baitap1.dart
@@ -0,0 +1,6 @@
+import 'package:baitap1/baitap1.dart' as baitap1;
+
+void main(List<String> arguments) {
+  print(
+      'Đây là kết quả buổi học thứ 2 về Dart: DART BASIC (phần 1) ${baitap1.calculate()}!');
+}
